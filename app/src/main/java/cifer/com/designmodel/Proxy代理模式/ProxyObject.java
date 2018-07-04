00e68bdb94c41a74a2ea4230dfd4b07951640b37
ciferxiao <1570058539@qq.com>
2018-07-04 11:29:22 +0800

    Proxy代理模式

--- app/src/main/java/cifer/com/designmodel/Proxy代理模式/ProxyObject.java
+++ app/src/main/java/cifer/com/designmodel/Proxy代理模式/ProxyObject.java
@@ -2,6 +2,7 @@ package cifer.com.designmodel.Proxy代理模式;
 
 /**
  * Created by cifer on 2018/6/12 10:35.
+ * 用处：可以在目标对象实现的基础上,增强额外的功能操作,即扩展目标对象的功能.
  * 代理对象角色
  * 代理主题-因为ProxySubject引用了RealSubject,并且实现了跟RealSubject一样的接口,
  * 所以ProxySubject可以操作RealSubject,还可以提供一些附加操作
